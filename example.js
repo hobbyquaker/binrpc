7a6eccda342adf9aec55bfd7f4951825114a8c08
hobbyquaker <hq@ccu.io>
2017-04-19 00:11:51 +0200

    fix example

--- example.js
+++ example.js
@@ -8,25 +8,30 @@
 var rpc = require('./lib/binrpc.js');
 
 // Config
-var thisHost = '172.16.23.134';
-var ccuHost = '172.16.23.3';
+var thisHost = '172.16.23.127';
+var ccuHost = '172.16.23.130';
 
 
-var rpcServer = rpc.createServer({host: thisHost, port: '2001'});    // Host running rpc server
+var rpcServer = rpc.createServer({host: thisHost, port: '2031'});    // Host running rpc server
 var rpcClient = rpc.createClient({host: ccuHost, port: '2001'});      // CCU
 
 rpcServer.on('system.listMethods', function (err, params, callback) {
     console.log(' <  system.listMethods');
-    callback(['system.listMethods', 'system.multicall', 'event', 'listDevices']);
+    callback(null, ['system.listMethods', 'system.multicall', 'event', 'listDevices']);
 });
 
 rpcServer.on('listDevices', function (err, params, callback) {
-    callback([]);
+    callback(null, []);
 });
 
 rpcServer.on('event', function (err, params, callback) {
-    console.log(' < event', params);
-    callback(['']);
+    console.log(' < event', JSON.stringify(params));
+    callback(null, '');
+});
+
+rpcServer.on('system.multicall', function (err, params, callback) {
+    console.log(' < system.multicall', JSON.stringify(params));
+    callback(null, '');
 });
 
 
@@ -39,9 +44,9 @@ rpcClient.on('connect', function () {
  * Tell the CCU that we want to receive events
  */
 function subscribe() {
-    console.log(' > ', 'init', ['xmlrpc_bin://' + thisHost + ':2001', 'test123']);
-    rpcClient.methodCall('init', ['xmlrpc_bin://' + thisHost + ':2001', 'test123'], function (err, res) {
-
+    console.log(' > ', 'init', ['xmlrpc_bin://' + thisHost + ':2031', 'test123']);
+    rpcClient.methodCall('init', ['xmlrpc_bin://' + thisHost + ':2031', 'test123'], function (err, res) {
+        console.log(err, res);
     });
 }
 
@@ -54,8 +59,14 @@ process.on('SIGINT', function () {
  * Tell the CCU that we no longer want to receive events
  */
 function unsubscribe() {
-    console.log(' > ', 'init', ['xmlrpc_bin://' + thisHost + ':2001', '']);
-    rpcClient.methodCall('init', ['xmlrpc_bin://' + thisHost + ':2001', ''], function (err, res) {
+    console.log(' > ', 'init', ['xmlrpc_bin://' + thisHost + ':2031', '']);
+    rpcClient.methodCall('init', ['xmlrpc_bin://' + thisHost + ':2031', ''], function (err, res) {
+        console.log(err, res);
+
         process.exit(0);
     });
+    setTimeout(function () {
+        console.log('force quit');
+        process.exit(1);
+    }, 1000);
 }
