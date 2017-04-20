a309f5b837b7ef0231da09d76bf13f722fa208ef
hobbyquaker <hq@ccu.io>
2017-04-20 15:31:38 +0200

    purge readme badges after build

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,10 @@ module.exports = function(grunt) {
         },
         shell: {
             coveralls: {
-                command: 'node_modules/.bin/nyc node_modules/.bin/mocha tests/*.js && node_modules/.bin/nyc report --reporter=text-lcov | node_modules/.bin/coveralls'
+                command: 'node_modules/.bin/nyc node_modules/.bin/mocha tests/*.js && node_modules/.bin/nyc report --reporter=text-lcov | node_modules/.bin/coveralls --force'
+            },
+            purgebadges: {
+                command: 'curl -X PURGE https://camo.githubusercontent.com/a554890f3c5ef62df1a95c58135b5e2fe82ad381/68747470733a2f2f636f766572616c6c732e696f2f7265706f732f6769746875622f686f6262797175616b65722f62696e7270632f62616467652e7376673f6272616e63683d6d6173746572 ; curl -X PURGE https://camo.githubusercontent.com/1634c087285b3ef4930287325ae07bce5329515a/68747470733a2f2f7472617669732d63692e6f72672f686f6262797175616b65722f62696e7270632e7376673f6272616e63683d6d6173746572'
             }
         }
     });
@@ -40,7 +43,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-shell');
-    grunt.registerTask('test', ['shell:coveralls', 'xo', 'simplemocha']);
+    grunt.registerTask('test', ['xo', 'shell:coveralls', 'simplemocha', 'shell:purgebadges']);
 
     grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
     grunt.registerTask('doc', ['jsdoc2md', 'concat']);
